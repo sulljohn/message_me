2ff6f6cda8ca83b055667632c521fe75c3c9c77d
John Sullivan <sulljohn12@gmail.com>
2019-11-24 15:39:10 -0500

    Better auth

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: "John", password: "password")
+User.create(username: "Greg", password: "password")
+User.create(username: "Craig", password: "password")
+User.create(username: "Leo", password: "password")
+User.create(username: "Karry", password: "password")
